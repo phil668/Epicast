2c6046500e03837dc9fed11437177c56e6ae0136
liuyue <1308972436@qq.com>
2023-11-06 23:13:52 +0800

    feat: mvp dev server

--- src/node/cli.ts
+++ src/node/cli.ts
@@ -1,16 +1,23 @@
+import { resolve } from 'node:path'
+import { cwd } from 'node:process'
 import { cac } from 'cac'
+import { createDevServer } from './dev'
 
 const cli = cac('epicast')
 
 cli.command('[root]', 'start dev server')
   .alias('dev')
-  .action((root) => {
-    console.log('dev root', root)
+  .action(async (root) => {
+    root = root ? resolve(root) : cwd()
+    const server = await createDevServer(root)
+    await server.listen()
+    server.printUrls()
   })
 
 cli.command('build [root]', 'start dev server')
-  .alias('dev')
+  .alias('build')
   .action((root) => {
+    // eslint-disable-next-line no-console
     console.log('dev root', root)
   })
 
